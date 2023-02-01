51ed438436eba45fc4706490621cc5c049a8ab8c
Codehash001 <codehashblockchain@gmail.com>
2023-02-01 21:31:51 +0530

    css fix

--- components/base.js
+++ components/base.js
@@ -235,7 +235,7 @@ const handleNav = () => {
        <div className='h-screen w-full bg-gray-200 flex pt-[60px]'>
         {/* veticle menu */}
           <div className='w-[120px] h-full flex flex-row items-center bg-yellow-300'> 
-            <span class={styles.container}><div className='justify-between'>
+            <span className={styles.container}><div className='justify-between'>
               <h1 className='mx-4'>Facebook</h1>
               <h1 className='mx-4'>Insta</h1>
               <h1 className='mx-4'>Discord</h1>
