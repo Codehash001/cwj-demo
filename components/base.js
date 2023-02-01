4f28c9eecfbca9991cc983b57cba7f56074c195c
Codehash001 <codehashblockchain@gmail.com>
2023-02-01 15:44:55 +0530

    fix

--- components/base.js
+++ components/base.js
@@ -236,7 +236,7 @@ const handleNav = () => {
           <div className='w-[200px] h-full bg-gray-500'></div>
         {/* main image */}
           <div className='w-full h-full bg-yellow-300'>
-            <div className='p-8 rounded-md'>
+            <div className='w-[600px] h-[800px] p-8 rounded-md'>
               <img src='/5.jpg' className='object-cover'/>
             </div>
           </div>
