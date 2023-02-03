381f6dc59f6216979693a5741583905391bd342a
Codehash001 <codehashblockchain@gmail.com>
2023-02-04 00:14:16 +0530

    css fix

--- components/about.js
+++ components/about.js
@@ -3,14 +3,14 @@ import React from 'react'
 function About() {
   return (
     <div>
-        <div className='w-full h-[250px] flex-flex-col items-center bg-blue-400 px-16'>
-           <h1 className='font-Archivo text-4xl text-center'>Welcome to Cafe ParadiseBean!</h1>
-           <div className='flex flex-row items-start justify-between'>
+        <div className='w-full h-[250px] flex-flex-col items-center bg-blue-400 pr-16 pl-[184px]'>
+           <h1 className='font-Archivo text-6xl text-center font-semibold my-8'>Welcome to Cafe ParadiseBean!</h1>
+           <div className='flex flex-row items-center my-5'>
             <img src='/5.jpg' className='w-[300px] h-auto'/>
-            <div className='px-6 py-3 rounded-md border'>Join our Discord and get listed for free mint</div>
+            <div className='px-6 py-3 rounded-md border font-Kanit text-3xl mx-6'>Join our Discord and get listed for free mint</div>
            </div> 
         </div>
-        <div className='h-full flex flex-col justify-end items-end'>
+        <div className='h-full flex flex-col justify-end items-end pr-16 pl-[184px]'>
            <div className='w-[300px]'>Lorem ipsum dolor sit amet, consectetur adipiscing elit,
             sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.
             Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris
