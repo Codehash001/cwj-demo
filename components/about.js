84e841fdf6f38041196f8d6d362340313a511b81
Codehash001 <codehashblockchain@gmail.com>
2023-02-03 23:14:42 +0530

    css fix

--- components/about.js
+++ components/about.js
@@ -10,8 +10,15 @@ function About() {
             <div className='px-6 py-3 rounded-md border'>Join our Discord and get listed for free mint</div>
            </div> 
         </div>
-        <div className='w-[300px] h-full flex flex-col items-end'>
-           Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum. 
+        <div className='h-full flex flex-col justify-end'>
+           <h1>Lorem ipsum dolor sit amet, consectetur adipiscing elit,
+            sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.
+            Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris
+            nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in
+            reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur.
+            Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia
+            deserunt mollit anim id est laborum.
+            </h1>
         </div>
     </div>
   )
