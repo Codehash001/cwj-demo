a317b9acc2c937f0f888e4609147b784ecedbdb7
Codehash001 <codehashblockchain@gmail.com>
2023-02-03 23:19:51 +0530

    css

--- components/about.js
+++ components/about.js
@@ -11,14 +11,14 @@ function About() {
            </div> 
         </div>
         <div className='h-full flex flex-col justify-end'>
-           <h1 className='W-[300px]'>Lorem ipsum dolor sit amet, consectetur adipiscing elit,
+           <div className='W-[300px]'>Lorem ipsum dolor sit amet, consectetur adipiscing elit,
             sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.
             Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris
             nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in
             reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur.
             Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia
             deserunt mollit anim id est laborum.
-            </h1>
+            </div>
         </div>
     </div>
   )
