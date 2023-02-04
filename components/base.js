af89254f0bd957bcfa3be0af2cbfc7c1faad3451
Codehash001 <codehashblockchain@gmail.com>
2023-02-04 17:06:21 +0530

    css fix

--- components/base.js
+++ components/base.js
@@ -228,7 +228,7 @@ const handleNav = () => {
     <div className="" id='home'data-aos="fade">
        <div className='h-screen w-full flex pt-[60px] pr-16'>
         {/* veticle menu */}
-          <div className='w-[120px] fixed z-[100] left-0 h-full flex flex-col items-center justify-center border-r-2 border-gray-900 bg-[#f8f5f5]'> 
+          <div className='w-[120px] fixed z-[100] h-full flex flex-col items-center justify-center border-r-2 border-gray-900 bg-[#f8f5f5]'> 
             <div className='flex -rotate-90 font-medium'>
               <h1 className='mx-4'>Facebook</h1>
               <h1 className='mx-4'>Instagram</h1>
