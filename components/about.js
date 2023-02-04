e8a3c5ffa90dbaff88676cedabc41a5413fc76ed
Codehash001 <codehashblockchain@gmail.com>
2023-02-04 10:41:35 +0530

    css fix

--- components/about.js
+++ components/about.js
@@ -7,19 +7,24 @@ function About() {
            <h1 className='font-Archivo text-6xl text-center font-semibold mb-8'>Welcome to Cafe ParadiseBean!</h1>
            <div className='flex flex-row items-start my-5'>
             <img src='/5.jpg' className='w-[300px] h-auto'/>
-            <div className='px-6 py-3 rounded-md border font-Kanit text-3xl mx-6'>Join our Discord and get listed for free mint</div>
-           </div> 
-        </div>
-        <div className='h-full flex flex-col justify-end items-end pr-16 pl-[184px]'>
-           <div className='w-[550px] text-xl font-Kanit'>Lorem ipsum dolor sit amet, consectetur adipiscing elit,
+            <div className='flex flex-col items-center mx-6'>
+            <div className='px-6 py-3 rounded-md border'>
+              <h1 className='font-Kanit text-3xl'>Join our Discord and get listed for free mint</h1>
+            </div>
+            <div className='w-[550px] pr-16 pl-[184px]'>
+           <p className='text-xl font-Kanit text-justify'>Lorem ipsum dolor sit amet, consectetur adipiscing elit,
             sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.
             Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris
             nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in
             reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur.
             Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia
             deserunt mollit anim id est laborum.
-            </div>
+            </p>
         </div>
+            </div>
+           </div> 
+        </div>
+
     </div>
   )
 }
