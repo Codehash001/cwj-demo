5be3d2791092a57592b03163428907db1089c826
Codehash001 <codehashblockchain@gmail.com>
2023-02-04 22:08:08 +0530

    css fix

--- components/about.js
+++ components/about.js
@@ -11,14 +11,11 @@ function About() {
             <div className='px-6 py-3 rounded-md border'>
               <h1 className='font-Kanit text-3xl'>Join our Discord and get listed for free mint</h1>
             </div>
-            <div className='w-[550px] mt-16'>
-           <p className='text-xl font-Kanit text-justify'>Lorem ipsum dolor sit amet, consectetur adipiscing elit,
-            sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.
-            Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris
-            nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in
-            reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur.
-            Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia
-            deserunt mollit anim id est laborum.
+            <div className='mt-16'>
+           <p className='text-xl font-Kanit text-justify'>We are the greatest place on earth you can enjoy that special cup of java with Jesus. What an incredible way to start your day off right. No matter what you're going through or where you are, He's always there with that special mug of morning goodness for you.
+Nothing says HAVE A GREAT DAY like Jesus sharing some relaxing time with you.Here at the cafe, we feature a majestic collection of 300 unique and artistic tokens, each with their own distinctive traits to make your day feel even better.
+
+
             </p>
         </div>
             </div>
@@ -28,14 +25,10 @@ function About() {
             <div className='flex flex-row items-start my-5'>
             
             <div className='flex flex-col items-center'>
-               <div className='w-[550px] mt-16'>
-           <p className='text-xl font-Kanit text-justify'>Lorem ipsum dolor sit amet, consectetur adipiscing elit,
-            sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.
-            Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris
-            nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in
-            reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur.
-            Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia
-            deserunt mollit anim id est laborum.
+               <div className='mt-16'>
+           <p className='text-xl font-Kanit text-justify'>
+Our collection was created for you to be a beacon of inspiration and positivity. The mission of the artist is to spread a grand message of courage, motivation and movement throughout the world. So come and enjoy a fresh cup of java with Jesus, and begin your day with a smile!Our vision is for our collection to bring the light of inspiration and the incentive to strive for happiness across the globe.
+Join us on the journey to enlighten the world on this mission.
             </p>
         </div>
             <div className='px-6 py-3 rounded-md border'>
