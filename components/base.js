6c84b192deaa1a7f8a661976d18a9f6b8203967a
Codehash001 <codehashblockchain@gmail.com>
2023-02-08 04:27:52 +0530

    update

--- components/base.js
+++ components/base.js
@@ -292,7 +292,7 @@ const handleNav = () => {
     {/* slide */}
       <About/>
       <Mintportal/>
-<div className='flex flex-row items-center justify-between overflow-x-hidden'> 
+<div className='flex flex-col items-center justify-between overflow-x-hidden'> 
       <Slider {...settings}>
        <div className='flex flex-col items-center justify-center mx-8'>  <img src='/5.jpg' className='w-[180px] h-[250px] rounded-md '/> </div>
        <div className='flex flex-col items-center justify-center mx-8'>  <img src='/5.jpg' className='w-[180px] h-[250px] rounded-md '/> </div>
