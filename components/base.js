064bc40326e970a4feb1a85e5f4c4170b03d00df
Codehash001 <codehashblockchain@gmail.com>
2023-02-08 04:17:45 +0530

    update

--- components/base.js
+++ components/base.js
@@ -141,8 +141,7 @@ const handleNav = () => {
         <meta name="Description" content="Coffe with Jesus - demo!" />
         <link rel="icon" href="/Logoicon.png" />
         
-       <link rel="stylesheet" type="text/css" charset="UTF-8" href="https://cdnjs.cloudflare.com/ajax/libs/slick-carousel/1.6.0/slick.min.css" /> 
-	<link rel="stylesheet" type="text/css" href="https://cdnjs.cloudflare.com/ajax/libs/slick-carousel/1.6.0/slick-theme.min.css" />
+       
       </Head>
       
     {/* nav */}
