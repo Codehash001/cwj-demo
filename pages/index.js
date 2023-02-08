4ef181884b3468aa86be870b841dca1d1c311752
Codehash001 <codehashblockchain@gmail.com>
2023-02-08 20:49:48 +0530

    css fix

--- pages/index.js
+++ pages/index.js
@@ -1,12 +1,7 @@
 import Head from 'next/head'
 import Image from 'next/image'
 import Navbar from '../components/mobilenav'
-import Main from '../components/main'
-import Slider from '../components/slide'
-import Contact from '../components/contact'
-import Footer from '../components/footer'
-import FAQs from '../components/FAQs'
-import Mint from '../components/mint'
+
 import Base from '../components/base'
 
 
