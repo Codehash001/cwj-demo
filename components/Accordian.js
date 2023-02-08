00df6b411af6b09306078346e3150f48387c3238
Codehash001 <codehashblockchain@gmail.com>
2023-02-09 00:20:52 +0530

    css fix

--- components/Accordian.js
+++ components/Accordian.js
@@ -6,22 +6,21 @@ export default function Accordian({id , title , content , active , setActive}) {
 
  
   return (
-    <div id='faqs'className='md:px-16 px-2 my-[44px] rounded-lg'data-aos="fade-up">
-      <h1 className='md:text-[40px] text-[30px] bg-gradient-to-r from-rose-600 via-red-700 to-red-600 bg-clip-text text-transparent font-Archivo text-center uppercase font-semibold tracking-wide mb-8'> Frequently Asked Questions</h1>
+    
     
     <div className="flex flex-col items-center w-full">
 
-      <div className="flex flex-row justify-between items-center bg-yellow-200 p-4">
+      <div className="flex flex-row justify-between items-center bg-yellow-200 p-4 w-full">
         <h1 className="text-lg font-semibold">{title}</h1>
-        <span>X</span>
+        <span onClick={()=>setActive(id)}>X</span>
       </div>
-
-      <div className='flex flex-col items-center p-6 bg-slate-500'>
+    
+      <div className={`flex flex-col items-center p-6 bg-slate-500${active === id ? "h-full ease-in duration-500": "h-0"}`}>
         <p className='font-Archivo text-lg font-medium'>{content}</p>
       </div>
 
     </div>
     
-    </div>
+    
   );
 }
--- components/FAQs.js
+++ components/FAQs.js
@@ -8,11 +8,14 @@ export default function Faqs() {
 
     return(
         <>
+        <div id='faqs'className='md:px-16 px-2 my-[44px] rounded-lg'data-aos="fade-up">
+            <h1 className='md:text-[40px] text-[30px] bg-gradient-to-r from-rose-600 via-red-700 to-red-600 bg-clip-text text-transparent font-Archivo text-center uppercase font-semibold tracking-wide mb-8'> Frequently Asked Questions</h1>
         {data.map ((item) =>
             <div key={item.id}>
-            <Accordian id={item.id} title={item.title} content={item.description}/>
+            <Accordian id={item.id} title={item.title} content={item.description} active={active} setActive={setActive}/>
             </div>
         )}
+        </div>
         </>
     )
 }
