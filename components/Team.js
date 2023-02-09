a0993e1547f29910b37642176553a6db98e78470
Codehash001 <codehashblockchain@gmail.com>
2023-02-09 19:18:41 +0530

    css fix

--- components/Team.js
+++ components/Team.js
@@ -3,32 +3,33 @@ import React from 'react'
 function Team() {
   return (
     <div className='flex flex-col items-center px-16'>
-        <h1 className='font-semibold font-Archivo text-5xl my-8'>Team</h1>
+        <h1 className='font-semibold font-Archivo text-5xl my-10 bg-gradient-to-r from-orange-500 to-red-700 bg-clip-text text-transparent'>Team</h1>
 
         <div className='flex flex-row items-center justify-between'>
             <div className='flex flex-col items-center p-6'>
-                <img src='/5.jpg' className='w-[200px] h-[200px]'/>
-                <h1 className='font-Archivo text-lg font-medium mt-3 mb-2'>Team Member</h1>
-                <h1 className='font-Archivo text-md font-medium'>Team Memberb Name</h1>
+                <img src='/owner.png' className='w-[300px] h-[300px]'/>
+                <h1 className='font-Archivo text-xl font-medium mt-3 mb-2 text-brand-01'>Team Member Name</h1>
+                <h1 className='font-Archivo text-lg font-medium'>Team Memberb</h1>
             </div>
 
             <div className='flex flex-col items-center p-6'>
-                <img src='/5.jpg' className='w-[200px] h-[200px]'/>
-                <h1 className='font-Archivo text-lg font-medium mt-6 mb-4'>Team Member</h1>
-                <h1 className='font-Archivo text-md font-medium'>Team Memberb Name</h1>
+                <img src='/discIng.png' className='w-[300px] h-[300px]'/>
+                <h1 className='font-Archivo text-xl font-medium mt-3 mb-2 text-brand-01'>Team Member Name</h1>
+                <h1 className='font-Archivo text-lg font-medium'>Team Memberb</h1>
             </div>
 
             <div className='flex flex-col items-center p-6'>
-                <img src='/5.jpg' className='w-[200px] h-[200px]'/>
-                <h1 className='font-Archivo text-lg font-medium mt-6 mb-4'>Team Member</h1>
-                <h1 className='font-Archivo text-md font-medium'>Team Memberb Name</h1>
+                <img src='/webdev.png' className='w-[300px] h-[300px]'/>
+                <h1 className='font-Archivo text-xl font-medium mt-3 mb-2 text-brand-01'>Team Member Name</h1>
+                <h1 className='font-Archivo text-lg font-medium'>Team Memberb</h1>
             </div>
 
             <div className='flex flex-col items-center p-6'>
-                <img src='/5.jpg' className='w-[200px] h-[200px]'/>
-                <h1 className='font-Archivo text-lg font-medium mt-6 mb-4'>Team Member</h1>
-                <h1 className='font-Archivo text-md font-medium'>Team Memberb Name</h1>
+                <img src='/socialM.png' className='w-[300px] h-[300px]'/>
+                <h1 className='font-Archivo text-xl font-medium mt-3 mb-2 text-brand-01'>Team Member Name</h1>
+                <h1 className='font-Archivo text-lg font-medium'>Team Memberb</h1>
             </div>
+
         </div>
     </div>
   )
