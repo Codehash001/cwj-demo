bd96005b09c408ee174cb96b249d2a2aa3015a50
Codehash001 <codehashblockchain@gmail.com>
2023-02-09 22:27:00 +0530

    css test

--- components/Accordian.js
+++ components/Accordian.js
@@ -2,14 +2,14 @@ import Raect, { useState, Fragment } from "react";
 
 
  
-export default function Accordian({id , title , content , active , setActive}) {
+export default function Accordian() {
 
  
   return (
     
     
     <div className="flex flex-col items-center">
-
+{/* 
       <div className="flex flex-row justify-between items-center bg-yellow-200 p-4 w-full">
         <h1 className="text-lg font-semibold">{title}</h1>
 
@@ -20,7 +20,7 @@ export default function Accordian({id , title , content , active , setActive}) {
       <div className={`flex flex-col items-center p-6 bg-slate-500 transition ease-in-out duration-500 ${active === id ? "h-full ": "opacity-0 h-0"}`}
      >
         <p className='font-Archivo text-lg font-medium'>{content}</p>
-      </div>
+      </div> */}
 
     </div>
     
