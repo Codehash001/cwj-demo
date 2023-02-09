c51bbab3fa5281c767ad2b1d2a24167d8bdb7adf
Codehash001 <codehashblockchain@gmail.com>
2023-02-09 16:12:25 +0530

    css fix

--- components/Roadmap.js
+++ components/Roadmap.js
@@ -83,7 +83,7 @@ function Roadmap() {
         </div>
 
         <div class="relative inline-flex items-center justify-start px-6 py-3 overflow-hidden font-medium transition-all bg-white rounded hover:bg-white group">
-        <span class="w-48 h-48 rounded rotate-[-40deg] bg-purple-600 absolute bottom-0 left-0 -translate-x-full ease-out duration-500 transition-all translate-y-full mb-9 ml-9 group-hover:ml-0 group-hover:mb-32 group-hover:translate-x-0"></span>
+        <span class="w-[600px] h-[600px] rounded-full rotate-[-40deg] bg-purple-600 absolute bottom-0 left-0 -translate-x-full ease-out duration-500 transition-all translate-y-full mb-9 ml-9 group-hover:ml-0 group-hover:mb-32 group-hover:translate-x-0"></span>
         <span class="relative w-full text-left text-black transition-colors duration-300 ease-in-out group-hover:text-white">
         <h1 className='text-2xl font-semibold text-brand-01 mb-2'>Phase seven</h1>
           <h1 className='text-xl font-semibold text-white mb-6'>TBA</h1>
