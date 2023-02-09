f4e6e2a5f2efdd4927645720ab58b99dbb127d54
Codehash001 <codehashblockchain@gmail.com>
2023-02-09 20:23:14 +0530

    css fix

--- components/Accordian.js
+++ components/Accordian.js
@@ -17,7 +17,7 @@ export default function Accordian({id , title , content , active , setActive}) {
         (<span onClick={()=>setActive(id)} className='cursor-pointer text-xl'>+</span>)}
       </div>
     
-      <div className={`flex flex-col items-center p-6 bg-slate-500 transition ease-in-out ${active === id ? "h-full opacity-0": "hidden h-0"}`}
+      <div className={`flex flex-col items-center p-6 bg-slate-500 transition ease-in-out ${active === id ? "h-full ": "opacity-0 h-0"}`}
      >
         <p className='font-Archivo text-lg font-medium'>{content}</p>
       </div>
