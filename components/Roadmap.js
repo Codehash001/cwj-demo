3e65e8cc7380f6a33a02dde693b9327e2d32dc81
Codehash001 <codehashblockchain@gmail.com>
2023-02-09 11:02:22 +0530

    css fix

--- components/Roadmap.js
+++ components/Roadmap.js
@@ -27,7 +27,7 @@ function Roadmap() {
         {/* slider */}
 <Slider {...settings}>
         <div className='p-6 flex flex-col items-center bg-gray-800 rounded-lg'>
-          <h1 className='text-lg font-semibold text-white mb-6'>Title here</h1>
+          <h1 className='text-xl font-semibold text-white mb-6'>Phase one</h1>
           <p className='text-gray-100 font-medium text-justify text-md'>
             Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
           </p>
